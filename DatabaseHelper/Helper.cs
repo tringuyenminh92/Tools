41c170359422cbd713daae9002f1ea0a39c60d31
tringuyenminh <tringuyenminh92@gmail.com>
2014-12-28 00:36:45 +0700

    Tools solution
    The solutions contains helper classes in coding.

---
+++ DatabaseHelper/Helper.cs
@@ -0,0 +1,96 @@
+﻿// ***********************************************************************
+// Assembly         : DatabaseHelper
+// Author           : MinhTri
+// Created          : 27-12-2014
+//
+// Last Modified By : MinhTri
+// Last Modified On : 28-12-2014
+// ***********************************************************************
+
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseHelper
+{
+    /// <summary>
+    /// Class Helper.
+    /// </summary>
+    public class Helper
+    {
+        /// <summary>
+        /// The _connection string
+        /// </summary>
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+
+        /// <summary>
+        /// Backups the database.
+        /// </summary>
+        /// <param name="backUpFile">The back up file.</param>
+        /// <param name="strDatabaseNameToBackup">The string database name to backup.</param>
+        /// <param name="csName">Name of the cs.</param>
+        /// <exception cref="System.Exception">Permission to perform this operation was denied - Choose a different folder to try again.</exception>
+        public static void BackupDatabase(string backUpFile, string strDatabaseNameToBackup = "QLBH_08_2014", string csName = "CS")
+        {
+            try
+            {
+                _connectionString = ConfigurationManager.ConnectionStrings[csName].ConnectionString;
+                SqlConnection.ClearAllPools();
+                SqlConnection connect = new SqlConnection(_connectionString);
+                connect.Open();
+                ServerConnection con = new ServerConnection(connect);
+                Server server = new Server(con);
+                Backup source1 = new Backup();
+                source1.Action = BackupActionType.Database;
+                source1.Database = strDatabaseNameToBackup;
+                Backup source = source1;
+                BackupDeviceItem destination = new BackupDeviceItem(backUpFile, DeviceType.File);
+                source.Devices.Add(destination);
+                source.Initialize = true;
+                source.Checksum = true;
+                source.ContinueAfterError = true;
+                source.Incremental = false;
+                source.LogTruncation = BackupTruncateLogType.Truncate;
+                source.SqlBackup(server);
+                connect.Close();
+            }
+            catch (FailedOperationException)
+            {
+                throw new Exception("Permission to perform this operation was denied - Choose a different folder to try again.");
+            }
+        }
+
+        /// <summary>
+        /// Restores the database.
+        /// </summary>
+        /// <param name="restoreFile">The restore file.</param>
+        /// <param name="strDatabaseNameToRestore">The string database name to restore.</param>
+        /// <param name="csName">Name of the cs.</param>
+        public static void RestoreDatabase(string restoreFile, string strDatabaseNameToRestore = "QLBH_08_2014", string csName = "CS")
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings[csName].ConnectionString;
+            SqlConnection connect = new SqlConnection(_connectionString);
+            connect.Open();
+            Server server = new Server(new ServerConnection(connect));
+            Restore restore1 = new Restore();
+            restore1.Database = strDatabaseNameToRestore;
+            Restore restore2 = restore1;
+            if (server.Databases[strDatabaseNameToRestore] != null)
+            {
+                server.KillAllProcesses(strDatabaseNameToRestore);
+            }
+            restore2.Devices.AddDevice(restoreFile, DeviceType.File);
+            restore2.ReplaceDatabase = true;
+            restore2.PercentCompleteNotification = 10;
+            restore2.SqlRestore(server);
+            server.Databases[strDatabaseNameToRestore].SetOnline();
+            connect.Close();
+        }
+    }
+}
---
+++ Excel/ImportExcel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel
+{
+    public class ImportExcel
+    {
+        private static string GetConnectionString(string pFilePath, bool headerRow = true)
+        {
+            string str = string.Empty;
+            string extension = Path.GetExtension(pFilePath);
+            string format;
+            if (extension == ".xls")
+            {
+                format = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties= \"Excel 8.0;HDR={1};\"";
+            }
+            else
+            {
+                if (!(extension == ".xlsx") && !(extension == ".xlsm"))
+                    throw new ArgumentOutOfRangeException("Excel file extenstion is not known.");
+                format = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR={1}\"";
+            }
+            return string.Format(format, (object)pFilePath, headerRow ? (object)"YES" : (object)"NO");
+        }
+
+        public static DataTable ExcelToDatatable(string path, string SheetName = "Sheet", string range = "A1:X65000")
+        {
+            DataTable dataTable = new DataTable();
+            using (OleDbConnection oleDbConnection = new OleDbConnection(GetConnectionString(path, true)))
+            {
+                try
+                {
+                    oleDbConnection.Open();
+                    string str = string.Format("SELECT * FROM [{0}${1}]", (object)SheetName, (object)range);
+                    OleDbCommand selectCommand = new OleDbCommand();
+                    selectCommand.CommandText = str;
+                    selectCommand.Connection = oleDbConnection;
+                    ((DbDataAdapter)new OleDbDataAdapter(selectCommand)).Fill(dataTable);
+                }
+                catch (Exception ex)
+                {
+                    oleDbConnection.Close();
+                    throw ex;
+                }
+            }
+            return dataTable;
+        }
+    }
+}
