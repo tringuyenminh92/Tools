7f8ebf7c7849f7f24bdf924443c22b50626a8e62
tringuyenminh <tringuyenminh92@gmail.com>
2015-06-13 22:00:38 +0700

    updated
    des

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using AES;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AESHelper helper = new AESHelper("45s.7z", "");
+          //helper.EncryptFile();
+            helper.DecryptFile();
+            Console.ReadLine();
+        }
+    }
+}
